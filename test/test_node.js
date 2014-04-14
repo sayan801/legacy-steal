969c92c70dff0d1974abec3d7c748e62dbb2891a
Justin Meyer <justinbmeyer@gmail.com>
2014-04-14 11:22:33 -0500

    added node test

--- test/test_node.js
+++ test/test_node.js
@@ -1,23 +1,91 @@
 var steal = require('../steal');
+	function throwError(e) {
+		setTimeout(function(){
+			throw e;
+		},1);
+	}
 
-console.log( __filename, __dirname);
-var printOut = function(config){
-	steal.System.ID ="evil"
-	var otherSteal = steal.clone( steal.addFormat( steal.System.clone() ) );
-	// config.baseURL
-	otherSteal.config(config);
-	var System = otherSteal.System;
-	System.ID ="good"
+    function traverse(object, iterator, parent, parentProperty) {
+      var key, child;
+      if (iterator(object, parent, parentProperty) === false)
+        return;
+      for (key in object) {
+        if (!object.hasOwnProperty(key))
+          continue;
+        if (key == 'location' || key == 'type')
+          continue;
+        child = object[key];
+        if (typeof child == 'object' && child !== null)
+          traverse(child, iterator, object, key);
+      }
+    }
+
+    // given a syntax tree, return the import list
+    function getImports(moduleTree) {
+      var imports = [];
+
+      function addImport(name) {
+        if ([].indexOf.call(imports, name) == -1)
+          imports.push(name);
+      }
+
+      traverse(moduleTree, function(node) {
+        // import {} from 'foo';
+        // export * from 'foo';
+        // export { ... } from 'foo';
+        // module x from 'foo';
+        if (node.type == 'EXPORT_DECLARATION') {
+          if (node.declaration.moduleSpecifier)
+            addImport(node.declaration.moduleSpecifier.token.processedValue);
+        }
+        else if (node.type == 'IMPORT_DECLARATION')
+          addImport(node.moduleSpecifier.token.processedValue);
+        else if (node.type == 'MODULE_DECLARATION')
+          addImport(node.expression.token.processedValue);
+      });
+      return imports;
+    }
+    
+    function getESModuleImports(load){
+    	load.address = load.address || 'anon' + ++anonCnt;
+        var parser = new traceur.syntax.Parser(new traceur.syntax.SourceFile(load.address, load.source));
+        var body = parser.parseModule();
+        return getImports(body);
+    }
+
+
+
+var trace = function(System, onFulfilled, onRejected){
 	var systemInstantiate = System.instantiate;
 	System.instantiate = function(load){
 		var res = systemInstantiate.apply(this, arguments);
-		if(res){
-			console.log("  >",load.name, res.deps)
-		} else {
-			console.log("  -",load.name)
-		}
+		Promise.resolve(res).then(function(instantiateResult){
+			if(!instantiateResult) {
+				var imports = getESModuleImports(load);
+				onFulfilled(load, imports );
+			} else {
+				onFulfilled(load, instantiateResult.deps);
+			}
+			
+		}, onRejected)["catch"](throwError);
+		
+
 		return res;
 	};
+};
+
+
+console.log( __filename, __dirname);
+var printOut = function(config){
+	var otherSteal = steal.clone( steal.addFormat( steal.System.clone() ) );
+	// config.baseURL
+	otherSteal.config(config);
+	trace(otherSteal.System, function(load, deps){
+		console.log("  >",load.name, load.metadata.format, deps);
+		
+		
+		
+	});
 	global.steal = otherSteal;
 	otherSteal.startup();
 
