9b5dd8c6d0f63d5bcc60637c995fbf4df99afd36
Matthew Phillips <matthew@matthewphillips.info>
2014-05-20 11:54:42 -0400

    Added option to startup to pass in a config
    This allows us to pass in a config object that will overwrite what was
    loaded in stealconfig.js

--- src/startup.js
+++ src/startup.js
@@ -83,7 +83,7 @@
 		return options;
 	};
 	
-	steal.startup = function(){
+	steal.startup = function(config){
 		
 		// get options from the script tag
 		if(global.document) {
@@ -128,6 +128,12 @@
 			configDeferred = steal.System.import("stealconfig");
 			
 			devDeferred = configDeferred.then(function(){
+				// If a configuration was passed to startup we'll use that to overwrite
+				// what was loaded in stealconfig.js
+				if(config) {
+					steal.config(config);
+				}
+
 				return steal("steal/dev");
 			},function(){
 				console.log("steal - error loading stealconfig.");
